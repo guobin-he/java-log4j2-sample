4485e415c697ffc5df8ae4b1519ab39f4b7e0af7
kakakikikeke <cdl.yoshinaka@gmail.com>
2015-05-15 15:40:37 +0900

    can be shown thread context in generated json

--- src/main/java/com/kakakikikeke/sample/Log4j2Sample.java
+++ src/main/java/com/kakakikikeke/sample/Log4j2Sample.java
@@ -2,6 +2,7 @@ package com.kakakikikeke.sample;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.ThreadContext;
 
 public class Log4j2Sample {
 
@@ -13,6 +14,10 @@ public class Log4j2Sample {
 	
 	public static void main(String[] args) {
 		new Log4j2Sample();
+		ThreadContext.put("domain", "hoge.com");
+		ThreadContext.put("port", "10001");
+		ThreadContext.put("username", "uername");
+		ThreadContext.put("password", "password");
 		log.error("This is error messages");
 		log.trace("下記以降はlog4j2.xmlファイルがクラスパスに通っていないと表示されません");
 		log.trace("This is trace messages");
